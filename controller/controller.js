56eb0db91e5ec2851f5fc25a2558c16bb0f2d6cf
Anshu000S <anshumanbaruah001@gmail.com>
2022-08-10 23:02:10 +0530

    nothing significant

--- controller/controller.js
+++ controller/controller.js
@@ -1,9 +1,23 @@
-const getHey = (req,res)=>{
+require('dotenv').config()
+const request = require('request')
+
+const getHey = (req, res) => {
     res.send(`<h1>THE SERVER IS RUNNING!!!</h1>`);
 };
 
-const getUser = (req,res)=>{
+const getUser = (req, res) => {
     res.send(`<h1>The name of the user is Anshuman Baruah!</h1>`)
 };
 
-module.exports = {getHey, getUser};
+const authToken = (req, res) => {
+    let client_id = process.env.client_id;
+    let client_secret = process.env.client_secret;
+}
+
+
+const getPlaylist = (req, res) => {
+    
+};
+
+
+module.exports = { getHey, getUser , authToken, getPlaylist };
--- router/router.js
+++ router/router.js
@@ -1,9 +1,10 @@
 const express  = require('express');
 const router   = express.Router();
 
-const { getHey, getUser} = require('../controller/controller.js')
+const { getHey, getUser, authToken, getPlaylist } = require('../controller/controller.js')
 
 router.route('/').get(getHey);
 router.route('/user').get(getUser);
-
+router.route('/auth').get(authToken);
+router.route('/userPlaylist').get(getPlaylist);
 module.exports = router
